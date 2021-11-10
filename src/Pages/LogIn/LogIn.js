958ef8045a2cc990646d694a761e074998e0e476
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-11-10 17:22:50 +0600

    now start work

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Col, Form, Row } from 'react-bootstrap';
 
 const LogIn = () => {
     return (
-        <div>
+        <div className='container'>
             <h2>Log in form</h2>
+            <Form >
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2" lg='4'>
+                        Email
+                    </Form.Label>
+                    <Col sm="10" lg='6'>
+                        <Form.Control className='ps-3'    placeholder="email@example.com" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2" lg='4'>
+                        Password
+                    </Form.Label>
+                    <Col sm="10" lg='6'>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+            </Form>
         </div>
     );
 };
--- src/Pages/LogIn/Registration/Registration.js
+++ src/Pages/LogIn/Registration/Registration.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Col, Form, Row } from 'react-bootstrap';
 
 const Registration = () => {
     return (
-        <div>
+        <div className='container'>
             <h2>Registration Form</h2>
+            <Form >
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+                    <Form.Label column sm="2">
+                        Name
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control placeholder="User Name" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">
+                        Email
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control placeholder="email@example.com" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Password
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextConformPassword">
+                    <Form.Label column sm="2">
+                        Conform Password
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="password" placeholder="Conform Password" />
+                    </Col>
+                </Form.Group>
+            </Form>
         </div>
     );
 };
