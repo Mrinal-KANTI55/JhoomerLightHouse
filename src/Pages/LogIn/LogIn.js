842e15fbb041cfe928f6ed2b8623d015397d006e
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-11-10 20:14:44 +0600

    work on user log in and regestion form

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 
 const LogIn = () => {
+    const [userInfo,setUserInfo]=useState({});
+    const WellComeUser=e=>{
+        const field =e.target.name;
+        const value = e.target.value;
+        const getInfo ={...userInfo};
+        getInfo[field]=value;
+        console.log(getInfo);
+        setUserInfo(getInfo);
+    }
     return (
         <div className='container'>
             <h2>Log in form</h2>
-            <Form >
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+            <Form>
+                <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2" lg='4'>
                         Email
                     </Form.Label>
                     <Col sm="10" lg='6'>
-                        <Form.Control className='ps-3'    placeholder="email@example.com" />
+                        <Form.Control className='ps-3' name='email' onBlur={WellComeUser}    placeholder="email@example.com" />
                     </Col>
                 </Form.Group>
 
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2" lg='4'>
                         Password
                     </Form.Label>
                     <Col sm="10" lg='6'>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" name='password' onBlur={WellComeUser}  placeholder="Password" />
                     </Col>
                 </Form.Group>
+                <Button variant="outline-success">Success</Button>
+
             </Form>
         </div>
     );
--- src/Pages/LogIn/Registration/Registration.js
+++ src/Pages/LogIn/Registration/Registration.js
@@ -1,44 +1,54 @@
-import React from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 
 const Registration = () => {
+    const [userInfo,setUserInfo]=useState({});
+    const RegisterNewUser=e=>{
+        const field =e.target.name;
+        const value = e.target.value;
+        const getInfo ={...userInfo};
+        getInfo[field]=value;
+        console.log(getInfo);
+        setUserInfo(getInfo);
+    }
     return (
         <div className='container'>
             <h2>Registration Form</h2>
             <Form >
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+                <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">
                         Name
                     </Form.Label>
                     <Col sm="10">
-                        <Form.Control placeholder="User Name" />
+                        <Form.Control onBlur={RegisterNewUser} name='User Name' placeholder="User Name" />
                     </Col>
                 </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">
                         Email
                     </Form.Label>
                     <Col sm="10">
-                        <Form.Control placeholder="email@example.com" />
+                        <Form.Control onBlur={RegisterNewUser} name='email' placeholder="email@example.com" />
                     </Col>
                 </Form.Group>
 
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">
                         Password
                     </Form.Label>
                     <Col sm="10">
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onBlur={RegisterNewUser} name='password' type="password" placeholder="Password" />
                     </Col>
                 </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextConformPassword">
+                <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">
                         Conform Password
                     </Form.Label>
                     <Col sm="10">
-                        <Form.Control type="password" placeholder="Conform Password" />
+                        <Form.Control onBlur={RegisterNewUser} name='ConformPassword' type="password" placeholder="Conform Password" />
                     </Col>
                 </Form.Group>
+                <Button variant="outline-success">Success</Button>
             </Form>
         </div>
     );
