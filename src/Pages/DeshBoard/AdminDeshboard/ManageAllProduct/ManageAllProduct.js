dff2a0bb87361519a6f831c617af2143e4149cee
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-11-12 02:02:17 +0600

    solve admin table problem

--- src/Pages/DeshBoard/AdminDeshboard/ManageAllProduct/ManageAllProduct.js
+++ src/Pages/DeshBoard/AdminDeshboard/ManageAllProduct/ManageAllProduct.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import TakeDecision from './TakeDecision/TakeDecision';
-
+import { Button, Table } from 'react-bootstrap';
 const ManageAllProduct = () => {
     const [Products, setProducts] = useState([]);
     useEffect(() => {
@@ -11,12 +10,49 @@ const ManageAllProduct = () => {
                 setProducts(res.data);
             });
     }, []);
+    const handelDelete = id => {
+        const conform = window.confirm('Are you sure to delete this Oreder?');
+        if (conform) {
+            const url = `https://fathomless-headland-44349.herokuapp.com/product/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted ');
+                    }
+
+                })
+
+        }
+    }
     return (
         <div>
             <h2 className='mt-5'>All Products Information </h2>
-            {
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>price</th>
+                        <th>Decision</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Products.map(ProductInfo => <tr key={ProductInfo._id} Product={ProductInfo}>
+                            <td>{ProductInfo.ProductName}</td>
+                            <td>{ProductInfo.price}</td>
+                            <td> <Button onClick={() => {
+                    handelDelete(ProductInfo._id)
+                }}>X</Button> </td>
+                        </tr>)
+                    }
+                </tbody>
+            </Table>
+            {/* {
                 Products.map(ProductInfo => <TakeDecision key={ProductInfo._id} Product={ProductInfo}></TakeDecision>)
-            }
+            } */}
 
         </div>
     );
